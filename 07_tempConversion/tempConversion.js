d7a7ade7d6ee59593d49578a1a4a1c97afec4470
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-01-05 17:41:25 +0700

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// (deg°F − 32) × 5/9 = deg°C
+const convertToCelsius = function(degF) {
+  let degC = (degF - 32) * 5/9;
+  if (degC === 0) return 0;
+  return parseFloat(degC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+// (deg°C × 9/5) + 32 = deg°F
+const convertToFahrenheit = function(degC) {
+  let degF = degC * 9/5 + 32;
+  if (degF === 0) return 0;
+  return parseFloat(degF.toFixed(1));
 };
 
 // Do not edit below this line
