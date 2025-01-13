fa6b6916db8fda9d802cb656d453469447deccaa
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-01-13 14:33:10 +0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (number >= 0) {
+    let sequence = [0, 1, 1];
+    for (let i = 3; i <= number; i++) {
+      sequence.push(sequence[i - 2] + sequence[i - 1]);
+    }
+    return sequence[number];
+  } else return "OOPS";
 };
 
 // Do not edit below this line
