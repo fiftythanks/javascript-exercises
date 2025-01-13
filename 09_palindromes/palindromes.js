553814eadfcec4fe354f170bba62db9becd56526
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-01-13 14:04:50 +0700

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.split("").filter((char) => {
+    if (
+      char === " "
+      || char === ","
+      || char === "."
+      || char === "!"
+      || char === "?"
+      || char === ";"
+      || char === ":"
+    ) {
+      return false;
+    } else return true;
+  }).join("");
+  reverseStr = str.split("").reverse().join("");
+  if (reverseStr.toLowerCase() === str.toLowerCase()) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
