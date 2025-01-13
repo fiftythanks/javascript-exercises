b96f93496a230fe93d219996cd2a85cf2c070cec
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-01-13 15:45:40 +0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = (new Date()).getFullYear();
 
+  people.sort((person1, person2) => {
+    let secondDate1;
+    let secondDate2;
+    if (person1.yearOfDeath) {
+      secondDate1 = person1.yearOfDeath;
+    } else {
+      secondDate1 = currentYear;
+    }
+
+    if (person2.yearOfDeath) {
+      secondDate2 = person2.yearOfDeath;
+    } else {
+      secondDate2 = currentYear;
+    }
+    
+    const age1 = secondDate1 - person1.yearOfBirth;
+    const age2 = secondDate2 - person2.yearOfBirth;
+    return age1 - age2;
+  });
+  return people.at(-1);
 };
 
 // Do not edit below this line
